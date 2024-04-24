926bafeeda2516eadce5bcbdb3bcd4d2f3a7a666
misterPython3 <yasomine.707@gmail.com>
2024-04-24 09:16:35 +0800

    yes

--- Artist Flair/main.js
+++ Artist Flair/main.js
@@ -10,23 +10,3 @@ navToggle.addEventListener("click", () => {
   primaryHeader.toggleAttribute("data-overlay");
 });
 
- // Function to shuffle an array (Fisher-Yates algorithm)
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  }
-
-  // Get all the .box elements
-  const boxes = document.querySelectorAll('.box');
-
-  // Convert NodeList to an array and shuffle it
-  const shuffledBoxes = shuffleArray(Array.from(boxes));
-
-  // Re-append the shuffled boxes to the exhibit container
-  const exhibitContainer = document.querySelector('.exhibit-container');
-  shuffledBoxes.forEach(box => {
-    exhibitContainer.appendChild(box);
-  });
